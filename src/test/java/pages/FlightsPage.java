03840003de139a061a0b8893c38bc533e31a56d3
IvanPetrovic991 <95961850+IvanPetrovic991@users.noreply.github.com>
2022-03-07 00:28:38 +0100

    refactor code

--- src/test/java/pages/FlightsPage.java
+++ src/test/java/pages/FlightsPage.java
@@ -14,61 +14,42 @@ import static org.testng.Assert.assertTrue;
 
 public class FlightsPage extends BasePage {
     WebDriver driver;
+    @FindBy(xpath = "//a[@data-decider-header='flights']")
+    WebElement flights;
+    @FindBy(xpath = "/html/body/div[6]/div/div[2]/div[1]/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]")
+    WebElement departInput;
+    @FindBy(xpath = "/html/body/div[6]/div/div[2]/div[1]/div[1]/div/div/div[1]/div[3]/div[1]/div/div[2]/div[1]")
+    WebElement returnInput;
+    @FindBy(xpath = "//div[contains(text(), 'adult')]")
+    WebElement clickAddAdult;
+    @FindBy(xpath = "/html/body/div[35]/div[2]/div/div/div[1]/div/div[2]/div/div/div[3]/button/span")
+    WebElement addAdult;
+    @FindBy(xpath = "/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div[2]/section[1]/div/h1")
+    WebElement background;
+    @FindBy(xpath = "/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div[2]/section[2]/div/div/div[2]/form[1]/div[1]/div/div[1]/div/div[3]/div/div/div")
+    WebElement clickToAddDestination;
+    @FindBy(xpath = "/html/body/div[5]/div/div[2]/div[1]/div[1]/input")
+    WebElement clickToTypeCity;
+    @FindBy(xpath = "//div[contains(text(), 'Vienna, Austria')][2]")
+    WebElement selectDestination;
+    @FindBy(xpath = "//div[contains(text(), 'Depart')][1]")
+    WebElement departButton;
+    @FindBy(xpath = "//div[contains(text(), 'Return')][1]")
+    WebElement flightType;
+    @FindBy(xpath = "//li[contains(text(), 'One-way')]")
+    WebElement selectOneWay;
+    @FindBy(xpath = "/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div[2]/section[2]/div/div/div[2]/form[1]/div[1]/div/div[2]/button")
+    WebElement clickSearchButton;
+    @FindBy (xpath = "/html/body/div[1]/div[1]/main/div/div[2]/div[2]/div/div[2]/div[1]/div[2]/div[4]/div[3]/div[1]/div/div[1]/div/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div[2]/div[2]/div/div/div")
+    WebElement seeFlightButton;
+    @FindBy (xpath = "/html/body/div[1]/div[1]/main/div/div[2]/div[1]/div/div[1]/div/form/div[1]/div[2]/div")
+    WebElement searchButton;
 
     public FlightsPage(WebDriver driver) {
         super(driver);
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-
-    @FindBy(xpath = "//a[@data-decider-header='flights']")
-    WebElement flights;
-
-    @FindBy(xpath = "/html/body/div[6]/div/div[2]/div[1]/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]")
-    WebElement departInput;
-
-    @FindBy(xpath = "/html/body/div[6]/div/div[2]/div[1]/div[1]/div/div/div[1]/div[3]/div[1]/div/div[2]/div[1]")
-    WebElement returnInput;
-
-    @FindBy(xpath = "//div[contains(text(), 'adult')]")
-    WebElement clickAddAdult;
-
-    @FindBy(xpath = "/html/body/div[35]/div[2]/div/div/div[1]/div/div[2]/div/div/div[3]/button/span")
-    WebElement addAdult;
-
-    @FindBy(xpath = "/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div[2]/section[1]/div/h1")
-    WebElement background;
-
-    @FindBy(xpath = "/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div[2]/section[2]/div/div/div[2]/form[1]/div[1]/div/div[1]/div/div[3]/div/div/div")
-    WebElement clickToAddDestination;
-
-    @FindBy(xpath = "/html/body/div[5]/div/div[2]/div[1]/div[1]/input")
-    WebElement clickToTypeCity;
-
-    @FindBy(xpath = "//div[contains(text(), 'Vienna, Austria')][2]")
-    WebElement selectDestination;
-
-    @FindBy(xpath = "//div[contains(text(), 'Depart')][1]")
-    WebElement departButton;
-
-    @FindBy(xpath = "//div[contains(text(), 'Return')][1]")
-    WebElement flightType;
-
-    @FindBy(xpath = "//li[contains(text(), 'One-way')]")
-    WebElement selectOneWay;
-
-    @FindBy(xpath = "/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div[2]/section[2]/div/div/div[2]/form[1]/div[1]/div/div[2]/button")
-    WebElement clickSearchButton;
-
-    @FindBy (xpath = "/html/body/div[1]/div[1]/main/div/div[2]/div[2]/div/div[2]/div[1]/div[2]/div[4]/div[3]/div[1]/div/div[1]/div/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div[2]/div[2]/div/div/div")
-    WebElement seeFlightButton;
-
-    @FindBy (xpath = "/html/body/div[1]/div[1]/main/div/div[2]/div[1]/div/div[1]/div/form/div[1]/div[2]/div")
-    WebElement searchButton;
-
-
-
-
 
     public void openFlightsPage() throws InterruptedException {
         clickElement(flights);
